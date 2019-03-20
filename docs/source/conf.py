124405b6f4f5af1373ab17bd32b0fdd0da22b569
NieneB <niene.boeijen@gmail.com>
2019-03-20 12:21:22 +0100

    dif theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -82,7 +82,7 @@ def setup(app):
 # a list of builtin themes.
 #
 html_logo = "cartiqo-circle.png"
-html_theme = 'bootstrap'
+html_theme = 'sphinx_rtd_theme'
 html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 
 # Theme options are theme-specific and customize the look and feel of a theme
